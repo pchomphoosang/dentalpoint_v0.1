2b659d23e2376673590ece76f502d6998dca6064
Pawat <pchomphoosang@gmail.com>
2015-04-08 10:07:07 +0700

    add new

--- public/assets/js/apps/search/list/list_view.js
+++ public/assets/js/apps/search/list/list_view.js
@@ -69,7 +69,7 @@ ContactManager.module("SearchApp.List", function(List, ContactManager, Backbone,
 
   List.Search = Marionette.ItemView.extend({
     template: "#search-panel",
-    className:"panel panel-default panel-search",
+    className:"panel panel-default panel-search ",
     events: {
       "click button.btn": "searchClicked"
     },
